49fce7cc9bdf970cab4aa0d5f4c80e6cc830c386
Jeferson Ferreira da Silva <jeferson.ferreira@unifesspa.edu.br>
2021-05-15 16:34:37 -0300

    Configurando modal de New Transaction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
+import Modal from 'react-modal';
 
-export function Header(){
+interface HeaderProps{
+    onOpenNewTransactionModal: () => void;
+}
+export function Header({onOpenNewTransactionModal}: HeaderProps){
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="j money" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
-                </button>          
+                </button>       
             </Content>  
         </Container>
     )
