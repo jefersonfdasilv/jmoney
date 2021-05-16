14103403232482fa346d75711015e8ebf395d57d
Jeferson Ferreira da Silva <jeferson.ferreira@unifesspa.edu.br>
2021-05-16 14:44:48 -0300

    Adiciona e estiliza os botoes de tipo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps{
     isOpen: boolean;
@@ -15,11 +18,11 @@ export function NewTransactionModal({isOpen,onRequestClose}: NewTransactionModal
           overlayClassName="react-modal-orverlay"
           className="react-modal-content"
       >
-        <button 
-            type="button" 
-            onClick={onRequestClose}
-            className="react-modal-close"
-        >
+            <button 
+                type="button" 
+                onClick={onRequestClose}
+                className="react-modal-close"
+            >
               <img src={closeImg} alt="Fechar modal" />
           </button>
           <Container>
@@ -31,6 +34,22 @@ export function NewTransactionModal({isOpen,onRequestClose}: NewTransactionModal
                 type="number"
                 placeholder="Valor"
             />
+            <TransactionTypeContainer>
+                <button
+                    type="button"
+                >
+                    <img src={incomeImg} alt="Entrada"/>
+                    <span>Entrada</span>
+                </button>
+
+                <button
+                    type="button"
+                >
+                    <img src={outcomeImg} alt="Saída"/>
+                    <span>Saída</span>
+                </button>
+
+            </TransactionTypeContainer>
             <input 
                 placeholder="Categoria"
             />
