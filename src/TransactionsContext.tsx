f1c22918a4a5a3f6b34c5180721d943fef609928
Jeferson Ferreira da Silva <jeferson.ferreira@unifesspa.edu.br>
2021-05-17 17:33:47 -0300

    Implementa o cadastro de transacoes com atualizacao da listagem

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -9,11 +9,17 @@ interface Transaction{
     amount: number;
     createdAt: string;
 }
+type TransactionInput = Omit<Transaction,'id'|'createdAt'>;
 
 interface TransactionsProviderProps{
     children: ReactNode;
 }
-export const TransactionsContext = createContext<Transaction[]>([]);
+
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
+}
+export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
 
 export function TransactionsProvider({children}: TransactionsProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -22,8 +28,22 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
             .then(response => setTransactions(response.data.transactions));
     },[]);
 
+    async function createTransaction(transactionInput: TransactionInput){
+        const response = await api.post('/transactions',transactionInput);
+        const {transaction} = response.data;
+
+        setTransactions([
+            ...transactions,
+            {
+                ...transaction,
+                createdAt: new Date(),
+            }
+        ]);
+
+
+    }
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{transactions, createTransaction}}>
             {children}
         </TransactionsContext.Provider>
     );
